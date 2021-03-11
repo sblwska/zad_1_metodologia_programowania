85f37a2ad6b72b8a0ff8005e19ec2b840b973c30
sblwska <80485318+sblwska@users.noreply.github.com>
2021-03-11 20:48:44 +0100

    Add files via upload

---
+++ test/test/Niezbednik.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Niezbednik
+    {
+        public double Wieksza(double a, double b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+
+            else return b;
+        }
+
+        public void Zamien(ref double a, ref double b)
+        {
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public Int32 Silnia(Int32 a)
+        {
+            Int32 res = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                res *= i;
+                if(a != 0 && res <= 0)
+                {
+                    return -1;
+                }
+            }
+
+            return res;
+        }
+
+        public Int32 RekurencyjnaSilnia(Int32 a)
+        {
+            if (a == 1) return 1;
+            return a * RekurencyjnaSilnia(a - 1);
+        }
+  
+    }
+
+
+
+
+
+
+
+}
---
+++ test/test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Niezbednik nzb = new Niezbednik();
+
+            Console.WriteLine("Podaj a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Wieksza jest liczba " + nzb.Wieksza(a, b) + "\n\n");
+            Console.WriteLine("Stare a = " + a + "\nStare b = " + b + "\n\n");
+
+            nzb.Zamien(ref a, ref b);
+
+            Console.WriteLine("Po zamianie: \na = " + a + "\nb = " + b + "\n\n");
+
+            try
+            {
+                Int32 a_ = Convert.ToInt32(a);
+                Int32 sil = nzb.Silnia(a_);
+
+                if (sil == -1)
+                {
+                    throw new StackOverflowException();
+                }
+
+                Console.WriteLine("Silnia liczby a (po zamianie) wynosi " + nzb.Silnia(a_));
+                Console.WriteLine("\nTa sama silnia liczona rekurencyjnie wynosi " + nzb.RekurencyjnaSilnia(a_));
+            }
+            catch (System.StackOverflowException)
+            {
+                Console.WriteLine("Blad przepelnienia. Nie oblicze silni z tej liczby.\n");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
